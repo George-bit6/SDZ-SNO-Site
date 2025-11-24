87ef1cf36dccf584fceec25fe5e34c4c7e6424f5
George-bit6 <155450799+George-bit6@users.noreply.github.com>
2025-11-24 17:58:20 +0200

    Change style of header in home page and add click interaction for scout groups

---
+++ script.js
@@ -0,0 +1,71 @@
+const scoutgroup = document.querySelectorAll(".scout-group")
+const cubs = document.getElementById("cubs")
+const scouts = document.getElementById("scouts") 
+const rovers = document.getElementById("rovers")
+const cubName = cubs.children[1]
+const scoutName = scouts.children[1]
+const roverName = rovers.children[1]
+const cubAge = cubs.children[2]
+const scoutAge = scouts.children[2]
+const roverAge = rovers.children[2]
+
+const cubsShirt = cubs.children[0]
+const scoutsShirt = scouts.children[0]
+const roversShirt = rovers.children[0]
+
+ let opacity = 0
+    const interval = 10
+    const steps = 80
+    const opacityDecrement = 1/80
+
+
+function fadeIn(image){
+
+    while(opacity <= 1){
+        opacity = opacity + steps
+   image.style.opacity = opacity
+    }
+
+}
+
+function resetElement(e1, e2 , e3){
+
+    e1.style.display = 'none'
+    e2.style.top = '400px' 
+    e3.style.top = '440px' 
+    
+}
+
+function setElement(e1, e2 , e3,){
+
+    e1.fadeIn
+    e1.style.display = 'block'
+    e2.style.top = '640px' 
+    e3.style.top = '680px'
+
+}
+
+
+cubs.addEventListener('click', ()=>{
+
+
+
+    setElement(cubsShirt, cubName, cubAge)    
+    resetElement(scoutsShirt, scoutName, scoutAge)
+    resetElement(roversShirt, roverName, roverAge)
+
+})
+scouts.addEventListener('click',()=>{
+
+    setElement(scoutsShirt, scoutName, scoutAge, scoutsShirt) 
+    resetElement(cubsShirt, cubName, cubAge)
+    resetElement(roversShirt, roverName, roverAge)
+
+
+})
+rovers.addEventListener('click',()=>{
+
+    setElement(roversShirt, roverName, roverAge, roversShirt) 
+    resetElement(cubsShirt, cubName, cubAge)
+    resetElement(scoutsShirt, scoutName, scoutAge)
+})
