58cfb899febab7048a51ebceb95cffc80b67c237
George-bit6 <georgeboufaysal@gmail.com>
2026-06-03 19:27:32 +0300

    remove all previous files

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import App from "./app/App.jsx";
-import "@fontsource/inter/400.css"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <ThemeProvider attribute="class" disableTransitionOnChange forcedTheme="light">
-        <App />
-      </ThemeProvider>
-    </ChakraProvider>
+    <App />
   </StrictMode>,
-);
+)
