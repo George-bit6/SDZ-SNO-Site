45d62491a80c748d46ba6842ede42a67f544b3d4
George-bit6 <georgeboufaysal@gmail.com>
2026-09-19 11:17:53 +0300

    remove redirects

--- .wrangler/tmp/bundle-AqSac7/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-AqSac7/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/home/gbf-unix/Home/SDZ-SNO-Site/.wrangler/tmp/bundle-mQI7h4/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/home/gbf-unix/Home/SDZ-SNO-Site/.wrangler/tmp/bundle-AqSac7/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/home/gbf-unix/.npm/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/home/gbf-unix/Home/SDZ-SNO-Site/.wrangler/tmp/bundle-mQI7h4/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/home/gbf-unix/Home/SDZ-SNO-Site/.wrangler/tmp/bundle-AqSac7/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/home/gbf-unix/Home/SDZ-SNO-Site/.wrangler/tmp/bundle-mQI7h4/middleware-insertion-facade.js";
+export * from "/home/gbf-unix/Home/SDZ-SNO-Site/.wrangler/tmp/bundle-AqSac7/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
